65ba4f98c755361611dd625068fad58c30344def
Shommon <sdtai2@outlook.com>
2023-11-27 17:30:35 -0500

    Added: Recursion operations work when enter is pressed multiple times Added: All Clear clears everything and resets global variables Added: Buffer Case in pushNum and changeOperator

--- script2.js
+++ script2.js
@@ -11,56 +11,149 @@ clearBtn.addEventListener('click', clearCurrent);
 let state = '';
 let currentNum = '0';
 let currentOperator = '';
-let sentence = ['','',''];
 let sentencePosition = -1;
+let recursionCount = 0;
+let recursionOn = false;
+let sentence = {
+    num1:'0',
+    operator:'',
+    num2:'',
+    result:''
+};
+
+
+//Initialize Display
+updateDisplay();
 
 
 
 // Calculator Button Listeners
 const buttons = document.querySelectorAll('#row')
 buttons.forEach((button) => button.addEventListener('click', (e) => {
-    changeState(e.target);
-    pushNum(e.target);
-    changeOperator(e.target);
+    if (e.target.id != 'row'){
+        changeState(e.target);
+        pushNum(e.target);
+        changeOperator(e.target);
+        grabResult(e.target);
+        updateDisplay();
 
-    updateDisplay();
+    }
 }))
+
 
 
 // Push Numbers
 function pushNum(button) {
     if (state == 'NUMERATE') {
-        if (sentencePosition == -1){
-            currentNum = '';
-            currentNum += button.id.toString();
-            sentencePosition = 0;
-            return
+        switch (sentencePosition) {
+            case -1: //Buffer
+                resetCurrentNum();
+                currentNum += button.id.toString(); //Push Numbers to String
+                sentencePosition = 0;
+                updateSentence('num1', currentNum);
+                return
+            case 0:
+                currentNum += button.id.toString(); //Push Numbers to String
+                updateSentence('num1', currentNum)
+                return
+            case 'buffer':
+                resetCurrentNum();
+                currentNum += button.id.toString(); //Push Numbers to String
+                sentencePosition = 2;
+                updateSentence('num2', currentNum)
+                return
+            // case 1: //Buffer
+            //     resetCurrentNum();
+            //     currentNum += button.id.toString(); //Push Numbers to String
+            //     sentencePosition = 2;
+            //     updateSentence('num2', currentNum)
+            //     return
+            case 2:
+                currentNum += button.id.toString();
+                updateSentence('num2', currentNum)
+                return
+
         }
-        currentNum += button.id.toString();
+    }
+}
+
+function updateSentence(category, input){
+    switch (category){
+        case 'num1':
+            sentence.num1 = input;
+            return;
+        case 'operator':
+            sentence.operator = input;
+            return
+        case 'num2':
+            sentence.num2 = input;
+            return
     }
 }
 
 // Change Operator
 function changeOperator(button) {
     if (state == 'OPERATION') {
-        sentencePosition = 1;
-
+        switch (sentencePosition){
+            case -1:
+                sentencePosition = 'buffer';
+                currentOperator = button.id.toString();
+                updateSentence('operator', currentOperator);
+            case 0:
+                sentencePosition = 'buffer';
+                currentOperator = button.id.toString();
+                updateSentence('operator', currentOperator);
+                return;
+            case 'buffer':
+                currentOperator = button.id.toString();
+                updateSentence('operator', currentOperator);
+                return
+            case 2:
+                previousValue(); // Set result as num1
+                currentOperator = button.id.toString(); //Update Current Operator
+                updateSentence('operator', currentOperator);
+                sentencePosition = 'buffer';
+                return;
+        }
     }
 }
 
-
-
-function updateDisplay() {
-    current.textContent = currentNum;
+function previousValue(){
+    sentence.num1 = sentence.result
 }
 
+//Display Functions
+function updateDisplay() {
+    if (state == 'RETURN'){
+        current.textContent = sentence.result;
+        previous.textContent = sentence.num1 + sentence.operator + sentence.num2;
+        return
+    }
+    switch (sentencePosition) {
+        case -1:
+            current.textContent = currentNum;
+            return
+        case 0:
+            current.textContent = sentence.num1;
+            return
+        case 'buffer':
+            current.textContent = currentNum;
+            previous.textContent = sentence.num1 + sentence.operator;
+            return
+        case 2: 
+            current.textContent = currentNum;
+            previous.textContent = sentence.num1 + sentence.operator + sentence.num2;
+            return
+    }
+    
+}
 
 // Operate
 function operate(array){
     let num1 = parseFloat(array[0]);
     let num2 = parseFloat(array[2]);
     let operator = array[1];
-    console.log(num1, operator ,num2);
+    // console.log(num1, operator ,num2);
     switch(operator) {
         case "+":
             return num1 + num2;
@@ -90,12 +183,73 @@ function changeState(button) {
     }
 }
 
+//Grab Result
+function grabResult(button){
+    // if (button.id == 'enter' && state == 'RETURN' && ) {
 
+    // } else if (button.id == 'enter' && state == 'RETURN'){
+    //     let result = operate(Object.values(sentence).slice(0,3));
+    //     console.log('pass')
+    //     sentence.result = result;
+    //     if (result == 0){
+    //         currentNum = '';
+    //     } else {
+    //         currentNum = result;
+    //     }
+    //     sentencePosition = 'buffer'
+    //     return
+    // } else {
+    //     returnstate = 'RECURSIVE'
+    // }
+
+    if (button.id == 'enter'){
+        let result;
+
+
+        if (recursionOn == false){
+            result = operate(Object.values(sentence).slice(0,3))
+            sentence.result = result;
+            recursionOn = true;
+        } else {
+            result = operate([sentence.result, sentence.operator, sentence.num2])
+            sentence.result = result;
+            recursionCount++;
+        }
+        if (result == 0){
+            currentNum = '';
+            } else {
+                currentNum = result;
+            }
+    }
+}
+
+// Reset Functions
 function clearAll(){
-    currentNum = '0';
-    current.textContent = ''
+    resetDisplay();
+    resetGlobalVariables();
 }
 
 function clearCurrent(){
+}
 
+function resetCurrentNum(){
+    currentNum = '';
+}
+
+function resetGlobalVariables(){
+    state = '';
+    currentNum = '0';
+    currentOperator = '';
+    sentencePosition = -1;
+    sentence = {
+    num1:'0',
+    operator:'',
+    num2:'',
+    result:''
+    };
+}
+
+function resetDisplay(){
+    current.textContent = '';
+    previous.textContent = '';
 }
